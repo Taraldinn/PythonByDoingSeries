7e6ede06369afb4dca45ce1ca4c2eb52b7531dbc
Taraldinn <kferdoush617@gmail.com>
2024-04-14 23:20:50 +0600

    Length and sum

--- intro python/main.py
+++ intro python/main.py
@@ -1,21 +1,9 @@
-# Advance sets operations
+#  Length and sum
 
-friends = {"rolf", "ruth", "charlie"} 
-guests = {"Jose", "bob", "rolf",  "michael"}
+grades = [10, 20, 30, 40, 50]
+total = sum(grades)
+length = len(grades)
+print(total, length)
 
-
-not_in_both = guests.symmetric_difference(friends)
-
-print(not_in_both)
-
-
-# Symmentic difference is the opposite of intersection
-
-
-print(guests.difference(friends))
-
-# difference is the opposite of intersection
-
-print(friends.intersection(guests))
-
-# intersection is used for taking common values 
+average = total / length
+print(average)
