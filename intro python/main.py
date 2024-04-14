7227a213ea3f91d000de8c81effce0ccc58942a9
Taraldinn <kferdoush617@gmail.com>
2024-04-14 23:17:18 +0600

    advance set operations

--- intro python/main.py
+++ intro python/main.py
@@ -1,10 +1,21 @@
-# Sets In python
+# Advance sets operations
 
-friends = ["rolf", "ruth", "charlie", ]
-guests = ["Jose", "bob", "rolf", "charli", "michael"]
+friends = {"rolf", "ruth", "charlie"} 
+guests = {"Jose", "bob", "rolf",  "michael"}
 
-friends.append("jen")
-print(friends)
 
-friends.remove("ruth")
-print(friends)
+not_in_both = guests.symmetric_difference(friends)
+
+print(not_in_both)
+
+
+# Symmentic difference is the opposite of intersection
+
+
+print(guests.difference(friends))
+
+# difference is the opposite of intersection
+
+print(friends.intersection(guests))
+
+# intersection is used for taking common values 
