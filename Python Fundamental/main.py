728b1a15036913978c2ea8648274d946ea79d023
Taraldinn <kferdoush617@gmail.com>
2024-07-21 22:40:43 +0600

    list comprehension done

--- Python Fundamental/main.py
+++ Python Fundamental/main.py
@@ -10,3 +10,16 @@ print(double_numbers)
 
 double_numbers = [ number * 3 for number in range(6) ]
 print(double_numbers)
+
+
+friend_ages = [22,31,35,37]
+
+age_strings= [f"My Friends is {age} years old" for age in friend_ages]
+print(age_strings)
+
+names = ["Rolf", "Bob", "Jen"]
+
+lower = [name.lower() for name in names]
+print(lower)
+
+
