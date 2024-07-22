b9eead8379b226d5b18007a1a2655064c5783306
Taraldinn <kferdoush617@gmail.com>
2024-07-22 06:11:08 +0600

    set comprehension with conditional

--- Python Fundamental/main.py
+++ Python Fundamental/main.py
@@ -1,18 +1,22 @@
+
+"""
 ages = [22,35,27,21,20]
 
 odds = [age for age in ages if age % 2 == 1  ]
 print(odds)
 
+"""
 
 friends = ["Rolf","Ruth","Charlie","jen"]
 guests = ["Bob", "Jose" , "Rolf", "Charlie", "Micheal"]
 
-friends_lower = set({f.lower() for f in friends})
-guest_lower = set({g.lower() for g in guests})
+friends_lower = {f.lower() for f in friends}
+guest_lower = {g.lower() for g in guests}
 
 print(friends_lower.intersection(guest_lower))
 
-present_friends = [
-    name.title() for name in guests if name.lower() in friends
-]
+present_friends = friends_lower.intersection(guest_lower)
+present_friends = {
+    name.title() for name in guests if name.lower() in present_friends
+}
 print(present_friends)
