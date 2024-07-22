17f17922e33bce83f118e9e1607bc59e4928aa35
Taraldinn <kferdoush617@gmail.com>
2024-07-22 06:33:16 +0600

    zip function

--- Python Fundamental/main.py
+++ Python Fundamental/main.py
@@ -6,11 +6,15 @@ guests = ["Bob", "Jose" , "Rolf", "Charlie", "Micheal"]
 time_since_seen = [3, 7, 15, 11]
 
 
-long_timers = {
-    friends[i]: time_since_seen[i]
-    for i in range(len(friends))
+# long_timers = {
+#     friends[i]: time_since_seen[i]
+#     for i in range(len(friends))
+#
+# }
+#
+# print(long_timers)
 
-}
 
+long_timers = dict(zip(friends, time_since_seen))
 print(long_timers)
 
