70d2ee5cc357a4cfa70ed7db5857e4635e75999b
Taraldinn <kferdoush617@gmail.com>
2024-07-28 17:34:02 +0600

    Arguments and Parameters

--- Python Fundamental/main.py
+++ Python Fundamental/main.py
@@ -1,10 +1,14 @@
+car = [
+    {"make": "Ford","model": "v1","mileage": 23000,"fuel_consumed": 460},
+    {"make": "Mazda","model": "v2","mileage": 23000,"fuel_consumed": 460},
+    {"make": "Mini","model": "v3","mileage": 23000,"fuel_consumed": 460},
+    {"make": "Roals Roils","model": "v4","mileage": 23000,"fuel_consumed": 460}
+]
 
-friends = ["Rolf","Ruth","Charlie","jen"]
 
+def calculateMpg(car_to_calculate):
+    mpg = car_to_calculate["mileage"] / car_to_calculate["fuel_consumed"]
+    name = f"{car_to_calculate['make']} {car_to_calculate['model']}"
+    print(f"{name} does {mpg} miles per Gallon")
 
-def greet():
-    name = input("Enter your name: ")
-    print(f"Hello, {name}!")
-
-greet()
-# print(name)
+calculateMpg(car[1])
