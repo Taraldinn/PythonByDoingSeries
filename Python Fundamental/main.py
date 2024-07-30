09836c44954fafcccf7900ad82f089fb1c9b4513
Taraldinn <kferdoush617@gmail.com>
2024-07-31 04:42:55 +0600

    functions and return values

--- Python Fundamental/main.py
+++ Python Fundamental/main.py
@@ -1,14 +1,27 @@
-car = [
-    {"make": "Ford","model": "v1","mileage": 23000,"fuel_consumed": 460},
-    {"make": "Mazda","model": "v2","mileage": 23000,"fuel_consumed": 460},
-    {"make": "Mini","model": "v3","mileage": 23000,"fuel_consumed": 460},
-    {"make": "Roals Roils","model": "v4","mileage": 23000,"fuel_consumed": 460}
+cars = [
+    {"make": "Ford", "model": "v1", "mileage": 23000, "fuel_consumed": 460},
+    {"make": "Mazda", "model": "v2", "mileage": 23000, "fuel_consumed": 460},
+    {"make": "Mini", "model": "v3", "mileage": 23000, "fuel_consumed": 460},
+    {"make": "Roals Roils", "model": "v4", "mileage": 23000, "fuel_consumed": 460}
 ]
 
 
-def calculateMpg(car_to_calculate):
-    mpg = car_to_calculate["mileage"] / car_to_calculate["fuel_consumed"]
-    name = f"{car_to_calculate['make']} {car_to_calculate['model']}"
-    print(f"{name} does {mpg} miles per Gallon")
+def calculate_mpg(car):
+    mpg = car["mileage"] / car["fuel_consumed"]
+    return mpg
 
-calculateMpg(car[1])
+
+def car_name(car):
+    name = f"{car['make']} {car['model']}"
+    return name
+
+
+def print_car_info(car):
+    name = car_name(car)
+    mpg = calculate_mpg(car)
+    print(f"{name} does {mpg} miles per Gallon")
+    return None
+
+for car in cars:
+    mpg = print_car_info(car)
+
