565086a333cbc385930e5abd6da2b609d37456dc
Taraldinn <kferdoush617@gmail.com>
2024-08-15 03:35:08 +0600

    OOP one Done

---
+++ Python Fundamental/practices/m3e1.py
@@ -0,0 +1,8 @@
+# Define a Movie class that has two properties: name and director
+
+class Movie:
+    def __init__(self, name, director):
+        self.name = name
+        self.name= director
+Movie = Movie('The Matrix', 'Wachowski')
+print(Movie)
